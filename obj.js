9f8f0de34d58d5c0d67a6bcf01bbe8112f176f40
alhanson <alhanson@dons.usfca.edu>
2017-11-19 00:38:29 -0800

    adding ambitions and a mindset

--- obj.js
+++ obj.js
@@ -1,6 +1,6 @@
 var people = [];
 
-function person(name, age, ethnicity, occupation, hometown, print) {
+function Person(name, age, ethnicity, occupation, hometown, print) {
 
         this.name = name;
         this.age = age;
@@ -8,10 +8,18 @@ function person(name, age, ethnicity, occupation, hometown, print) {
         this.occupation = occupation;
         this.hometown = hometown;
         // this.person = person;
-        // people.protoype.push(person);
+        // people.prototype.push(person);
         this.print = function () {
-          return "Hi, my name is" + " " + name;
+          return "Hi, my name is" + " " + this.name;
         }
         //people[people.length] = this.person;    
 }
-var Alex = new person("Alex Hanson", "18", "black", "student", "gardena");
+
+var Alex = new Person("Alex Hanson", "18", "black", "student", "gardena");
+
+Person.prototype.ambitions = "dreams";
+
+Person.prototype.mindset = function () {
+	return "What are my ambitions?" + "They are evolving" + this.ambitions;
+};
+
